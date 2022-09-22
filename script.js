a995c4a1c11a84d1a828de86a2ec9a4f4049081f
Ana Carolina Godoi <81260565+AnaGodoi@users.noreply.github.com>
2022-09-22 16:18:49 -0300

    Adicionado transições

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function mudaTema() {
   document.body.classList.toggle("dark");
 }
+
+document.querySelectorAll(".projetos-item").forEach((item) =>
+  item.addEventListener("click", function (e) {
+    item.querySelector(".codepen-container").classList.toggle("open");
+  })
+);
